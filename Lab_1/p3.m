6af16664d2404b49b703376eb459a0fe0b295c5e
rayvo50 <72882602+rayvo50@users.noreply.github.com>
2021-12-11 23:56:25 +0000

    n sei o que fazer bros

--- Lab_1/p3.m
+++ Lab_1/p3.m
@@ -1,18 +1,55 @@
-%correr primeiro p1
-u = zeros(1,360);
-v = zeros(1,360);
+clear
+close all
+
 a = 0.09;
-KT = 10;
+Kt = 10;
 b = 1;
-v(1) = 0;
-h=1;
-for i= 1:length(u)
-    u(i) = c2(i)/(7.1903+c2(i));
+
+% syms v
+% 
+% % Equacoes a resolver
+% Equacao = a*v*(1-v/Kt) == 0;
+% Equacao_pos = a*v*(1-v/Kt) > 0;
+% Equacao_neg = a*v*(1-v/Kt) < 0;
+% 
+% % Soluções das equações
+% V_eq = solve(Equacao, v);
+% V_pos = solve(Equacao_pos, v, 'ReturnConditions', true);
+% V_neg = solve(Equacao_neg, v, 'ReturnConditions', true);
+% 
+% % escrever os resultados
+% disp('Pontos no equilíbrio:')
+% disp(V_eq)
+% disp('Intervalo em que a derivada é positiva:')
+% disp(V_pos.conditions)
+% disp('Intervalo em que a derivada é negativa:')
+% disp(V_neg.conditions)
+
+% determinar V(h) tal que
+% V'(h) =  a*V*(1-V/Kt) - b*u*V
+% passo:
+h = 0.1;
+V = zeros(1, 10/h+1);
+
+%testar cenas
+V(1) = 5; %?
+u = 0.5
+for k = 1:length(V)-1
+   V(k+1) = V(k) + h*(a*V(k)*(1-V(k)/Kt) - b*u*V(k));
 end
+plot(1:length(V), V)
 
-
-for k = 1:length(v)
-    v(k+1) = v(k * h)+ h*(a*v(k)*(1-(v(k)/KT))-b*u(k)*v(k));
-end
-
-
+% para testar tudo
+% for n = 1:10
+%     figure(n)
+%     % cond. ini.: 
+%     V(1) = n; %?
+%     for u = 0:0.01:1
+%         for k = 1:length(V)-1
+%             V(k+1) = V(k) + h*(a*V(k)*(1-V(k)/Kt) - b*u*V(k));
+%         end
+%         plot(1:length(V), V)
+%         hold on
+% 
+%     end
+% end
