458a13d19dc4d0398a1e66d6e6679ecf9a5da1ac
Midas <goncalomidoes@tecnico.ulisboa.pt>
2021-12-15 15:41:14 +0000

    p2 in a function

--- Lab_1/p2.m
+++ Lab_1/p2.m
@@ -1,23 +1,21 @@
-Num_dias = 90
+function [u] = p2(dosagem,dias,dias_entre_dosagem)
 
-c2 = zeros(1,Num_dias);
-[~,c2] = p1_vers2(0,0,2,Num_dias,6);
+    c2 = zeros(1,dias);
+    [~,c2] = p1_vers2(0,0,dosagem,dias,dias_entre_dosagem);
 
-u = zeros(1,Num_dias);
-v = zeros(1,Num_dias);
-a = 0.09;
-KT = 10;
-b = 1;
-h=1;
-for x=1:10:50
-    [~,c2] = p1_vers2(0,0,x,Num_dias,6);
-    for i= 1:length(u)
+    u = zeros(1,dias);
+%     for x=1:10:50
+%         [~,c2] = p1_vers2(0,0,x,dias,dias_entre_dosagem);
+%         for i= 1:length(u)
+%             u(i) = c2(i)/(7.1903+c2(i));
+%         end
+%         %plot(1:1:Num_dias,u(:));
+%         hold on
+%     end
+    
+      for i= 1:length(u)
         u(i) = c2(i)/(7.1903+c2(i));
-    end
-    plot(1:1:Num_dias,u(:));
-    hold on
-end
+      end
 
 
-
-%plot(1:1:Num_dias,u(:));
+    %plot(1:1:Num_dias,u(:));
