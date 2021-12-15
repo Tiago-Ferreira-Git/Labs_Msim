bf454a7f6f1fbcc63d58d757e67243d175d4bc4b
Tiago <72828463+TiagoWebMaster@users.noreply.github.com>
2021-12-15 22:27:34 +0000

    Added comments and fixed minor things in p2 and p1_vers2

--- Lab_1/p1_vers2.m
+++ Lab_1/p1_vers2.m
@@ -1,6 +1,27 @@
-function [c1,c2] = p1_vers2(c1_inicial,c2_inicial,dosagem,dias,dias_entre_dosagem)
-    %Constantes e Vetor d como definido no enunciado%
-    d = upsample(zeros(1,dias)+dosagem,dias_entre_dosagem); 
+%Função p2
+%   Variáveis de entrada:
+%       -c1_inicial: condição inicial para a concentração 1       
+%       -c2_inicial: condição inicial para a concentração 2
+%       -dosagem: dose (em mg) administrada        
+%       -dias: Período de tempo a observar (em dias)
+%       -dias_entre_dosagem: Período de tempo entre cada toma do fármaco (em dias)
+%       -d: variável como definida no enunciado (para p5 passar como argumento um array não nulo)
+%    Variáveis de saída:
+%       -c1: Array de floats referente à concentração 1       
+%       -c1: Array de floats referente à concentração 2    
+
+
+
+function [c1,c2] = p1_vers2(c1_inicial,c2_inicial,dosagem,dias,dias_entre_dosagem,d)
+    
+    %Declaração dos vetores a utilizar.%
+    if size(d) < 1        
+        d = upsample(zeros(1,dias)+dosagem,dias_entre_dosagem); 
+    end
+    X = zeros(2,(dias)-1); %X contém os valores de c1 na primeira linha e c2 na segunda linha
+    
+    
+    %Dados do problema%
     h=1;
     K12 = 0.3 * 3600;
     K21 = 0.2455 * 3600;
@@ -8,24 +29,27 @@ function [c1,c2] = p1_vers2(c1_inicial,c2_inicial,dosagem,dias,dias_entre_dosage
     V1 = 3110;
     V2 = 3110;
     delta = 1000;
-    X = zeros(2,(dias*dias_entre_dosagem)-1);
+    
     %condições iniciais%
     X(1,1) = c1_inicial;
     X(2,1) = c2_inicial;
-    length(X(1,:));
-    Matrix = zeros(2,2);
-    Matrix = [(1/V1)*(-K12-K10),(1/V1)*K21;(1/V2)*K12,(-1/V2)*K21];
     
+    
+    Matriz = zeros(2,2);
+    Matriz = [(1/V1)*(-K12-K10),(1/V1)*K21;(1/V2)*K12,(-1/V2)*K21];
+    
+    d = d(1:dias);
     d = d*(1/V1)*delta;
 
     for k = 1:length(X(1,:)-1)
-        X(:,k+1) = (X(:,k*h)+ h*(Matrix*([X(1,k);X(2,k)])));
+        X(:,k+1) = (X(:,k*h)+ h*(Matriz*([X(1,k);X(2,k)])));
         X(1,k+1) = X(1,k+1) + d(k);
 
     end
     c1 = X(1,:);
     c2 = X(2,:);
     
-    %plot(1:1:dias*dias_entre_dosagem,X(1,:),1:1:dias*dias_entre_dosagem,X(2,:),1:1:dias*dias_entre_dosagem,d);
+    plot(0:1:dias-1,X(1,:),0:1:dias-1,X(2,:),0:1:dias-1,d);
     % figure(1)
+end
 
--- Lab_1/p2.m
+++ Lab_1/p2.m
@@ -1,21 +1,32 @@
-function [u] = p2(dosagem,dias,dias_entre_dosagem)
+%Função p2
+%   Variáveis de entrada:
+%       -dosagem: dose (em mg) administrada        
+%       -dias: Período de tempo a observar (em dias)
+%       -dias_entre_dosagem: Período de tempo entre cada toma do fármaco (em dias)
+%   
+%    Variável de saída:
+%       Array de floats correspondente ao parâmetro u como definido no
+%       enunciado
 
+
+function [u] = p2(dosagem,dias,dias_entre_dosagem)
+    
     c2 = zeros(1,dias);
-    [~,c2] = p1_vers2(0,0,dosagem,dias,dias_entre_dosagem);
+    [~,c2] = p1_vers2(0,0,dosagem,dias,dias_entre_dosagem,[]);
 
     u = zeros(1,dias);
-%     for x=1:10:50
-%         [~,c2] = p1_vers2(0,0,x,dias,dias_entre_dosagem);
-%         for i= 1:length(u)
-%             u(i) = c2(i)/(7.1903+c2(i));
-%         end
-%         %plot(1:1:Num_dias,u(:));
-%         hold on
-%     end
-    
-      for i= 1:length(u)
-        u(i) = c2(i)/(7.1903+c2(i));
-      end
-
-
+    %Comentar caso não seja necessário reproduzir os gráficos%
+%    for x=1:10:50
+%        [~,c2] = p1_vers2(0,0,x,dias,dias_entre_dosagem,[]);
+%        for i= 1:length(u)
+%            u(i) = c2(i)/(7.1903+c2(i));
+%        end
+%        plot(1:1:dias,u);
+%        hold on
+%    end
+     for i= 1:length(u)
+       u(i) = c2(i)/(7.1903+c2(i));
+     end
     %plot(1:1:Num_dias,u(:));
+end
+   
