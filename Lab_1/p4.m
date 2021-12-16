90b19b0dd703220df358d13c47f8326a95bea087
fabexdias <fabiomagdias@hotmail.com>
2021-12-16 10:30:01 +0000

    P4 modificado

--- Lab_1/p4.m
+++ Lab_1/p4.m
@@ -2,8 +2,8 @@ close all
 clear
 
 %Variaveis de controlo
-max_mg = 30;
-max_space = 40;
+max_mg = 20;
+max_space = 10;
 
 %Parametros dados
 a = 0.09;
@@ -23,7 +23,7 @@ for jj = 1:max_mg
     figure(jj)
     
     %Loop que descobre os diferentes andamentos de volume quando o volume
-    %inicial é 1mm^3 e se toma ll mg de ii dias em ii dias
+    %inicial é 1mm^3 e se toma jj mg de ii dias em ii dias
     for ii = 1:max_space
         u = p2(jj,100,ii);
         for k = 1:length(V)-1
@@ -32,8 +32,14 @@ for jj = 1:max_mg
         plot(0:length(V(ii,:))-1, V(ii,:));
         hold on
     end
-    new_min = 1000;
+    xlabel('Time (days)')
+    ylabel('Volume (mm^3)')
+    figure(jj+100);
+    plot(0:length(u)-1, u*100);
+    xlabel('Time (days)')
+    ylabel('Efect (%)')   
     
+    new_min = 1000;
     %Loop que descobre para que valor de espaçamento se obtem 10% (ou mais
     %próximo disso) do valor inicial de volume ao fim de 25 dias
     for ii = 1:max_space
@@ -52,4 +58,3 @@ axis([0 max_mg 0 min(max_space + 5,30)]);
 xlabel('Dose (mg)')
 %Tempo entre dosagens para que o tamanho seja 10% ao fim de 25 dias
 ylabel('Spacing between doses (days)') 
-
