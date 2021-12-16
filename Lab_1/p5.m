eb2b783126aced94a951263d5b84907427c3bf42
Tiago Ferreira <tiagombferreira@hotmail.com>
2021-12-16 17:52:46 +0000

    teste

--- Lab_1/p5.m
+++ Lab_1/p5.m
@@ -1,4 +1,5 @@
 dosagem = 3;
+dias = 5
 %Dados do enunciado
 a = 0.09;
 Kt = 10;
@@ -7,35 +8,15 @@ h = 1;
 
 
 
-r = randi([0 6],1,50);
 %d = zeros(1,sum(r,'all'));
-d = zeros(1,100);
+d = zeros(1,1);
+d(1) = 3
 index=0;
-for i= 1:1:100
-    if i < 20
-        d(i) = 1;
-        continue
-    end
-    if i < 40
-        d(i) = 2;
-        continue
-    end
-    if i < 60
-        d(i) = 3;
-        continue
-    end
-    if i < 80
-        d(i) = 4;
-        continue
-    end
-    if i <= 100
-        d(i) = 5;
-        continue
-    end
+n = 1:6;
+teste = fibonacci(n)
+for i= 1:6
+    d = horzcat(d,upsample(zeros(1,5)+dosagem,i))
 end
-
-% for i= 1:1:length(r)
-%     index = r(i) + index;
-%     d(index) = dosagem;
-% end
-[~,c2] = p1_vers2(0,0,dosagem,100,2,d);
+d
+%D = 
+%[~,c2] = p1_vers2(0,0,dosagem,100,2,d);
