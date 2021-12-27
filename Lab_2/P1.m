b5d856ec7f8515c1383930a91023d950417eb331
Tiago <72828463+TiagoWebMaster@users.noreply.github.com>
2021-12-27 23:29:54 +0000

    Added the vectors as stated cenas

--- Lab_2/P1.m
+++ Lab_2/P1.m
@@ -1,9 +1,9 @@
-Njogadas=5;
+Njogadas=100;
 NMC =7; %Número de runs
 Ndiscard=0;
 Ncasas=0;
 z = zeros(NMC,7); %número de estados do jogo, que indica o número de vezes que se caiu em cada casa
-y = zeros(NMC,Njogadas); % dimensão igual ao número de jogadas em cada run de Monte Carlo, que indica as casas em que se caiu em cada jogada
+y = zeros(1,Njogadas); % dimensão igual ao número de jogadas em cada run de Monte Carlo, que indica as casas em que se caiu em cada jogada
 Aluguer = [10,10,0,15,20,25,0,35];
 estados = [2,3,4,5,6,3,1;3,4,5,6,3,7,2];
 
@@ -13,9 +13,10 @@ for i=1:1:NMC
         z(i,x) = z(i,x)+1;
         avanca = randi([1 2]);
         x = estados(avanca,x);
-        y(i,k) = x;
+        y(1,k) = x;
     end 
     z(i,x) = z(i,x)+1;
     
 end
-z
+%to do: pág 7 do pdf
+bar(z)
