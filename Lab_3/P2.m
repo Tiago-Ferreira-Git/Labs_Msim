8f0c8aa9eb324dd38f14c981c1b2ccefb28d8ea1
fabexdias <fabiomagdias@hotmail.com>
2022-01-23 11:57:32 +0000

    1st by me

--- Lab_3/P2.m
+++ Lab_3/P2.m
@@ -1,13 +1,13 @@
 clear
 close all;
-open_system('P2_sim');
+open_system('P2_sim_bex');
 
 %Encontrar a cena do zeno do P1
 %set_param('P1_sim', 'ZeroCrossAlgorithm','Nonadaptive');
 
-set_param('P2_sim', 'ZeroCrossAlgorithm','Adaptive');
+set_param('P2_sim_bex', 'ZeroCrossAlgorithm','Adaptive');
 
-set_param('P2_sim', 'StopTime','25');
+set_param('P2_sim_bex', 'StopTime','25');
 
 warning('off', 'Simulink:Solver:ZeroCrossingNotBracketedDueToSmallSignalValues');
 
@@ -15,11 +15,11 @@ atrito=0;
 velocidade = 0;
 
 %Variar a elasticidade%
-set_param('P2_sim/Velocidade_inicial','Value','0');
-set_param('P2_sim/Elasticidade_da_bola','Gain','-0.8');
+set_param('P2_sim_bex/Velocidade_inicial','Value','0');
+set_param('P2_sim_bex/Elasticidade_da_bola','Gain','-0.8');
 for jj=1:1:8
-    set_param('P2_sim/Atrito_do_ar','Gain',num2str(atrito));
-    out=sim('P2_sim', 'SaveTime', 'on', 'SaveState', 'on');
+    set_param('P2_sim_bex/Atrito_do_ar','Gain',num2str(atrito));
+    out=sim('P2_sim_bex', 'SaveTime', 'on', 'SaveState', 'on');
     ti=out.tout;
     xi=out.z;
     axis([0 12 0 12]);
