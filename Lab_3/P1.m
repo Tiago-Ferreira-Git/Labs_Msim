ecf773ba15d7905295b9f8e3f15b36ec7dd4f33b
Tiago <72828463+TiagoWebMaster@users.noreply.github.com>
2022-01-23 17:10:31 +0000

    changed appearence of plots

--- Lab_3/P1.m
+++ Lab_3/P1.m
@@ -14,19 +14,7 @@ warning('off', 'Simulink:Solver:ZeroCrossingNotBracketedDueToSmallSignalValues')
 elasticidade =-0.8;
 velocidade = -6;
 
-
-cores = [[0 0.4470 0.7410];
-    [0.8500 0.3250 0.0980];
-    [0.9290 0.6940 0.1250];
-    [0.4940 0.1840 0.5560];
-    [0.4660 0.6740 0.1880];
-    [0.3010 0.7450 0.9330];
-    [0.6350 0.0780 0.1840];
-    [0 0.4470 0.7410];
-    ];
-
 %Variar a elasticidade%
-
 set_param('P1_sim/Velocidade_inicial','Value','0');
 for jj=1:1:8
     set_param('P1_sim/Elasticidade_da_bola','Gain',num2str(elasticidade));
@@ -34,10 +22,11 @@ for jj=1:1:8
     ti=out.tout;
     xi=out.z;
     axis([0 12 0 12]);
-    legend();
+    lgd = legend;
+    lgd.FontSize = 12;
     xlabel('Tempo - s')
     ylabel('Posição - m')
-    plot(xi.Time,xi.data,'DisplayName', sprintf('Elasticidade %0.1f',elasticidade));
+    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Elasticidade %0.1f',elasticidade));
     hold all;
     elasticidade = elasticidade + 0.1;
     %   Tfinal = Tfinal*0.8;
@@ -56,10 +45,11 @@ for jj=1:1:7
     ti=out.tout;
     xi=out.z;
     axis([0 16 0 14])
-    legend();
+    lgd = legend;
+    lgd.FontSize = 12;
     xlabel('Tempo - s')
     ylabel('Posição - m')
-    plot(xi.Time,xi.data,'DisplayName', sprintf('Velocidade %d',velocidade));
+    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Velocidade %d',velocidade));
     hold all;
     velocidade = velocidade + 2;
     pause(0.1)
@@ -67,7 +57,7 @@ for jj=1:1:7
 end
 hold off
 
-%% 0.0005
+% 0.0005
 set_param('P1_sim', 'StopTime','60');
 figure
 set_param('P1_sim/Elasticidade_da_bola','Gain','-0.8');
