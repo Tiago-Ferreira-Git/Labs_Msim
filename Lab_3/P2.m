56f64e40dcb4b2b0ca419232bd0b88e6b9e6cb3d
Tiago <72828463+TiagoWebMaster@users.noreply.github.com>
2022-01-23 10:28:42 +0000

    fabex

--- Lab_3/P2.m
+++ Lab_3/P2.m
@@ -1,22 +1,35 @@
 clear
 close all;
-open_system('P2');
-set_param('P2', 'ZeroCrossAlgorithm','Adaptive');
-set_param('P2', 'StopTime','25');
-warning('off', 'Simulink:Solver:ZeroCrossingNotBracketedDueToSmallSignalValues');
-out=sim('P2', 'SaveTime', 'on', 'SaveState', 'on');
-ti=out.tout;
-xi=out.z;
+open_system('P2_sim');
 
-figure
-Tfinal=25;
-axis([0 Tfinal 0 25])
-hold on
-curve=animatedline;
-set(gca,'Xlim',[0 Tfinal])
-for ii=1:length(xi.Time)
- addpoints(curve,xi.Time(ii),xi.data(ii));
- drawnow
- pause(0.01)
+%Encontrar a cena do zeno do P1
+%set_param('P1_sim', 'ZeroCrossAlgorithm','Nonadaptive');
+
+set_param('P2_sim', 'ZeroCrossAlgorithm','Adaptive');
+
+set_param('P2_sim', 'StopTime','25');
+
+warning('off', 'Simulink:Solver:ZeroCrossingNotBracketedDueToSmallSignalValues');
+
+atrito=0;
+velocidade = 0;
+
+%Variar a elasticidade%
+set_param('P2_sim/Velocidade_inicial','Value','0');
+set_param('P2_sim/Elasticidade_da_bola','Gain','-0.8');
+for jj=1:1:8
+    set_param('P2_sim/Atrito_do_ar','Gain',num2str(atrito));
+    out=sim('P2_sim', 'SaveTime', 'on', 'SaveState', 'on');
+    ti=out.tout;
+    xi=out.z;
+    axis([0 12 0 12]);
+    legend();
+    xlabel('Tempo - s')
+    ylabel('Posição - m')
+    plot(xi.Time,xi.data,'DisplayName', sprintf('Atrito %0.1f',atrito));
+    hold all;
+    atrito = atrito + 0.1;
+    pause(0.1)
+    disp(atrito)
 end
-hold off
+hold off;
