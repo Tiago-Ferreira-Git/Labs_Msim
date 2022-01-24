919a505a6407c0329d04d76c9773a2e9f4ce6fb6
fabexdias <fabiomagdias@hotmail.com>
2022-01-24 16:34:46 +0000

    4th by me

--- Lab_3/P2.m
+++ Lab_3/P2.m
@@ -1,81 +1,58 @@
 clear
 close all;
-open_system('P2_sim');
+open_system('P2_sim_bex');
 
-%Encontrar a cena do zeno do P1
-%set_param('P1_sim', 'ZeroCrossAlgorithm','Nonadaptive');
+set_param('P2_sim_bex', 'ZeroCrossAlgorithm','Adaptive');
 
-set_param('P2_sim', 'ZeroCrossAlgorithm','Adaptive');
-
-set_param('P2_sim', 'StopTime','25');
+set_param('P2_sim_bex', 'StopTime','25');
 
 warning('off', 'Simulink:Solver:ZeroCrossingNotBracketedDueToSmallSignalValues');
 
-atritop=0;
-atritom=0;
+atrito=0;
 velocidade = 0;
 
 %Variar o atrito%
-set_param('P2_sim/Velocidade_inicial','Value',num2str(velocidade));
-set_param('P2_sim/Elasticidade_da_bola','Gain','-0.8');
+set_param('P2_sim_bex/Velocidade_inicial','Value',num2str(velocidade));
+set_param('P2_sim_bex/Elasticidade_da_bola','Gain','-0.8');
 for jj=1:1:3
-    set_param('P2_sim/Atrito_do_ar+','Gain',num2str(atritom));
-    out=sim('P2_sim', 'SaveTime', 'on', 'SaveState', 'on');
+    set_param('P2_sim_bex/Atrito_do_ar','Gain',num2str(atrito));
+    out=sim('P2_sim_bex', 'SaveTime', 'on', 'SaveState', 'on');
     ti=out.tout;
+    
+    figure(1)
+    grid on;
     xi=out.z;
     axis([0 12 0 12]);
     lgd = legend;
     lgd.FontSize = 12;
     xlabel('Tempo - s')
     ylabel('Posição - m')
-    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Atrito %0.1f',atritom));
+    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Atrito %0.1f',atrito));
     hold all;
-    atritom = atritom - 0.4;
-    pause(0.1)
-end
-hold off;
-
-figure
-%Variar o atrito%
-set_param('P2_sim/Velocidade_inicial','Value',num2str(velocidade));
-set_param('P2_sim/Elasticidade_da_bola','Gain','-0.8');
-atritom = 0;
-for jj=1:1:3
-    set_param('P2_sim/Atrito_do_ar+','Gain',num2str(atritom));
-    out=sim('P2_sim', 'SaveTime', 'on', 'SaveState', 'on');
-    ti=out.tout;
+    
+    figure(2)
+    grid on;
     xi=out.v;
-    axis([0 12 -10 10]);
+    axis([0 12 -15 12]);
     lgd = legend;
     lgd.FontSize = 12;
     xlabel('Tempo - s')
     ylabel('Velocidade - m/s')
-    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Atrito %0.1f',atritom));
+    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Atrito %0.1f',atrito));
     hold all;
-    atritom = atritom - 0.4;
-    pause(0.1)
-end
-hold off;
-
-
-figure
-%Variar o atrito%
-set_param('P2_sim/Velocidade_inicial','Value',num2str(velocidade));
-set_param('P2_sim/Elasticidade_da_bola','Gain','-0.8');
-atritom = 0;
-for jj=1:1:3
-    set_param('P2_sim/Atrito_do_ar+','Gain',num2str(atritom));
-    out=sim('P2_sim', 'SaveTime', 'on', 'SaveState', 'on');
-    ti=out.tout;
+    
+    figure(3)
+    grid on;    
     xi=out.a;
     axis([0 12 -16 0]);
     lgd = legend;
     lgd.FontSize = 12;
     xlabel('Tempo - s')
     ylabel('Acelaração - m/s^2')
-    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Atrito %0.1f',atritom));
+    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Atrito %0.1f',atrito));
     hold all;
-    atritom = atritom - 0.4;
+    
+    atrito = atrito - 0.4;
     pause(0.1)
 end
 hold off;
