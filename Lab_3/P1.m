6dca4d148e3c095b24afbbdb226a89d28765cd61
Tiago Ferreira <tiagombferreira@hotmail.com>
2022-02-02 16:56:59 +0000

    Fabio acaba

--- Lab_3/P1.m
+++ Lab_3/P1.m
@@ -17,19 +17,22 @@ velocidade = -10;
 %Variar a elasticidade%
 set_param('P1_sim/Velocidade_inicial','Value','0');
 figure(1)
-numero_de_maximos = 7;
+numero_de_maximos = 1;
 alt_max = zeros(numero_de_maximos,numero_de_maximos);
 for jj=1:1:numero_de_maximos
     set_param('P1_sim/Elasticidade_da_bola','Gain',num2str(elasticidade));
     out=sim('P1_sim', 'SaveTime', 'on', 'SaveState', 'on');
     ti=out.tout;
     xi=out.z;
-    axis([0 12 0 12]);
-    lgd = legend;
+    axis([12.857 12.8573 0 5e-09]);
+    hold on;
+    %12.85714286
+    plot(12.85714,0,'b*','DisplayName','T_{infito}')
+    hold on;    lgd = legend;
     lgd.FontSize = 12;
     xlabel('Tempo - s')
     ylabel('Posição - m')
-    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Elasticidade %0.1f',abs(elasticidade)));
+    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Elasticidade %0.1f',abs(elasticidade)),'color',[0, 0.4470 ,0.7410]);
     hold all;
     elasticidade = elasticidade + 0.1;
     pause(0.01)
@@ -46,58 +49,58 @@ for jj=1:1:numero_de_maximos
     end
 end
 
-height_quocient = zeros(numero_de_maximos,length(alt_max(1,:))-1);
-gaussian_params = zeros(2,numero_de_maximos);
-for jj=1:1:numero_de_maximos
-    for i=1:1:length(alt_max(1,:))-1
-        if alt_max(jj,i+1)/alt_max(jj,i) == 0
-            height_quocient(jj,i) = NaN;
-        else
-            height_quocient(jj,i) = alt_max(jj,i+1)/alt_max(jj,i);
-        end
-        
-    end
-    %standart deviation
-    gaussian_params(1,jj) = std(height_quocient(jj,:),'omitnan');
-    %mean
-    gaussian_params(2,jj) = mean(height_quocient(jj,:),'omitnan');
-end
+% height_quocient = zeros(numero_de_maximos,length(alt_max(1,:))-1);
+% gaussian_params = zeros(2,numero_de_maximos);
+% for jj=1:1:numero_de_maximos
+%     for i=1:1:length(alt_max(1,:))-1
+%         if alt_max(jj,i+1)/alt_max(jj,i) == 0
+%             height_quocient(jj,i) = NaN;
+%         else
+%             height_quocient(jj,i) = alt_max(jj,i+1)/alt_max(jj,i);
+%         end
+%         
+%     end
+%     %standart deviation
+%     gaussian_params(1,jj) = std(height_quocient(jj,:),'omitnan');
+%     %mean
+%     gaussian_params(2,jj) = mean(height_quocient(jj,:),'omitnan');
+% end
+% 
+% hold off;
+% %Variar a velocidade inicial%
+% elasticidade =-0.8;
+% set_param('P1_sim/Elasticidade_da_bola','Gain','-0.8');
+% figure(2)
+% for jj=1:1:5
+%     
+%     set_param('P1_sim/Velocidade_inicial','Value',num2str(velocidade));
+%     out=sim('P1_sim', 'SaveTime', 'on', 'SaveState', 'on');
+%     ti=out.tout;
+%     xi=out.z;
+%     axis([0 16 0 14])
+%     lgd = legend;
+%     lgd.FontSize = 12;
+%     xlabel('Tempo - s')
+%     ylabel('Posição - m')
+%     plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Velocidade %d',velocidade));
+%     hold all;
+%     velocidade = velocidade + 3;
+%     pause(1)
+%     disp(velocidade);
+% end
+% hold off
 
-hold off;
-%Variar a velocidade inicial%
-elasticidade =-0.8;
-set_param('P1_sim/Elasticidade_da_bola','Gain','-0.8');
-figure(2)
-for jj=1:1:5
-    
-    set_param('P1_sim/Velocidade_inicial','Value',num2str(velocidade));
-    out=sim('P1_sim', 'SaveTime', 'on', 'SaveState', 'on');
-    ti=out.tout;
-    xi=out.z;
-    axis([0 16 0 14])
-    lgd = legend;
-    lgd.FontSize = 12;
-    xlabel('Tempo - s')
-    ylabel('Posição - m')
-    plot(xi.Time,xi.data,'LineWidth',1.5,'DisplayName', sprintf('Velocidade %d',velocidade));
-    hold all;
-    velocidade = velocidade + 3;
-    pause(1)
-    disp(velocidade);
-end
-hold off
-
-% 0.0005
-set_param('P1_sim', 'StopTime','60');
-figure(3)
-set_param('P1_sim/Elasticidade_da_bola','Gain','-0.8');
-out=sim('P1_sim', 'SaveTime', 'on', 'SaveState', 'on');
-ti=out.tout;
-xi=out.z;
-plot(xi.Time,xi.data,'DisplayName', sprintf('Elasticidade %0.1f',elasticidade));
-axis([57.90 57.95 0 0.2*1e-4]);
-xlabel('Tempo - s')
-ylabel('Posição - m')
+% %0.0005
+% set_param('P1_sim', 'StopTime','60');
+% figure(3)
+% set_param('P1_sim/Elasticidade_da_bola','Gain','-0.8');
+% out=sim('P1_sim', 'SaveTime', 'on', 'SaveState', 'on');
+% ti=out.tout;
+% xi=out.z;
+% plot(xi.Time,xi.data,'DisplayName', sprintf('Elasticidade %0.1f',elasticidade));
+% axis([57.90 57.95 0 0.2*1e-4]);
+% xlabel('Tempo - s')
+% ylabel('Posição - m')
 
 
 
